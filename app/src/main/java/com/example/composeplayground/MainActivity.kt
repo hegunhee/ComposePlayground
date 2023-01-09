b228381e2e5becb505bc779eb820bba8b0ed616b
이건희 <gunhee0072@gmail.com>
2023-01-09 18:10:15 +0900

    [TODO-LIST] 버튼을 달아서 아이템 추가, 초기화하는 코드 추가

--- app/src/main/java/com/example/composeplayground/MainActivity.kt
+++ app/src/main/java/com/example/composeplayground/MainActivity.kt
@@ -2,22 +2,17 @@ package com.example.composeplayground
 
 import android.content.Context
 import android.os.Bundle
-import android.widget.Space
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.contextaware.ContextAware
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.composeplayground.ui.theme.ComposePlaygroundTheme
@@ -27,14 +22,42 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposePlaygroundTheme {
+                var itemList by remember {mutableStateOf(0) }
+                val increaseItem : () -> Unit = {
+                    itemList++ 
+                }
+                val resetItem : () -> Unit = {
+                    itemList = 0
+                }
                 val context = LocalContext.current
-                LazyColumn(modifier = Modifier) {
-                    items(100){
-                        DataText(number = it,context)
+                if(itemList == 0){
+                    ZeroItemList(increaseItem)
+                }else{
+                    Column() {
+                        Row(Modifier.height(100.dp)){
+                            Button(onClick = increaseItem,modifier = Modifier.padding(end = 10.dp)) {
+                                Text(text = "it is $itemList")
+                            }
+                            Button(onClick = resetItem) {
+                                Text(text = "reset Item")
+                            }
+                        }
+
+                        LazyColumn(){
+                            items(itemList){
+                                DataText(number = it+1, context = context)
+                            }
+                        }
                     }
                 }
             }
         }
+    }
+}
+@Composable
+fun ZeroItemList(onClick : () -> Unit){
+    Button(onClick = onClick){
+        Text(text = "zero Item")
     }
 }
 
@@ -45,12 +68,23 @@ fun DataText(number : Int,context : Context) {
             text = "Title $number",
             modifier = Modifier
                 .fillMaxWidth()
-                .clickable { Toast.makeText(context, "Title $number", Toast.LENGTH_SHORT).show() },
+                .clickable {
+                    Toast
+                        .makeText(context, "Title $number", Toast.LENGTH_SHORT)
+                        .show()
+                },
             fontSize = 30.sp,
         )
         Text(text = "subTitle $number", modifier = Modifier
             .fillMaxWidth()
-            .clickable { Toast.makeText(context, "SubTitle $number", Toast.LENGTH_SHORT).show() }, fontSize = 15.sp)
-        Spacer(Modifier.fillMaxWidth().height(30.dp))
+            .clickable {
+                Toast
+                    .makeText(context, "SubTitle $number", Toast.LENGTH_SHORT)
+                    .show()
+            }, fontSize = 15.sp)
+        Spacer(
+            Modifier
+                .fillMaxWidth()
+                .height(30.dp))
     }
 }
