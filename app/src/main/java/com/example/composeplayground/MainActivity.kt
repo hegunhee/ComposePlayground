a7cc8e513dcf17f046ae926b937c53f4848c7abc
이건희 <gunhee0072@gmail.com>
2023-01-09 17:49:35 +0900

    [TODO-LIST] 클릭 리스너 장착

--- app/src/main/java/com/example/composeplayground/MainActivity.kt
+++ app/src/main/java/com/example/composeplayground/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.example.composeplayground
 
+import android.content.Context
 import android.os.Bundle
+import android.widget.Space
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.activity.contextaware.ContextAware
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.MaterialTheme
@@ -11,7 +16,10 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composeplayground.ui.theme.ComposePlaygroundTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,9 +27,10 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposePlaygroundTheme {
+                val context = LocalContext.current
                 LazyColumn(modifier = Modifier) {
-                    items(10){
-                        DataText(text = "$it Data")
+                    items(100){
+                        DataText(number = it,context)
                     }
                 }
             }
@@ -30,6 +39,18 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun DataText(text : String) {
-    Text(text = text)
+fun DataText(number : Int,context : Context) {
+    Column(modifier = Modifier.fillMaxWidth()) {
+        Text(
+            text = "Title $number",
+            modifier = Modifier
+                .fillMaxWidth()
+                .clickable { Toast.makeText(context, "Title $number", Toast.LENGTH_SHORT).show() },
+            fontSize = 30.sp,
+        )
+        Text(text = "subTitle $number", modifier = Modifier
+            .fillMaxWidth()
+            .clickable { Toast.makeText(context, "SubTitle $number", Toast.LENGTH_SHORT).show() }, fontSize = 15.sp)
+        Spacer(Modifier.fillMaxWidth().height(30.dp))
+    }
 }
