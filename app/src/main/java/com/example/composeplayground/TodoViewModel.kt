445952db53de0235ff006b4e0255158eb23ce7d8
이건희 <gunhee0072@gmail.com>
2023-01-11 14:07:36 +0900

    [TODO-LIST] dialog 관련 코드도 viewModel로 이전

--- app/src/main/java/com/example/composeplayground/TodoViewModel.kt
+++ app/src/main/java/com/example/composeplayground/TodoViewModel.kt
@@ -1,12 +1,16 @@
 package com.example.composeplayground
 
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import com.example.composeplayground.entity.TodoEntity
 
 class TodoViewModel : ViewModel() {
 
     var todoList =  mutableStateListOf<TodoEntity>()
+    private set
+
+    var dialogOpen = mutableStateOf<Boolean>(false)
     private set
 
     fun addTodo(todo : TodoEntity){
@@ -21,6 +25,14 @@ class TodoViewModel : ViewModel() {
         val index = todoList.indexOf(todoList.find { it == todo })
         todoList.removeAt(index)
         todoList.add(index,TodoEntity(todo.todo,!todo.isChecked))
+    }
+
+    fun dismissDialog() {
+        dialogOpen.value = false
+    }
+
+    fun openDialog() {
+        dialogOpen.value = true
     }
 
 
