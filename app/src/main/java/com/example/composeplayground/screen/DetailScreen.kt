ef0a454c78c735be1f0bb9aa8be85efa27d69fca
hegunhee <gunhee0072@gmail.com>
2023-03-30 21:23:24 +0900

    refactor. DetailScreenRoute를 만들어 viewModel을 분리해줌

--- app/src/main/java/com/example/composeplayground/screen/DetailScreen.kt
+++ app/src/main/java/com/example/composeplayground/screen/DetailScreen.kt
@@ -9,32 +9,53 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.composeplayground.components.BackButton
 import com.example.composeplayground.components.DeleteButton
+import com.example.composeplayground.detail.DetailViewModel
 import com.example.domain.model.Todo
 
 @Composable
-fun DetailScreen(onClickBack : () -> Unit, onClickDelete : () -> Unit, todo : Todo) {
-    Column(modifier = Modifier.fillMaxWidth().padding(start = 20.dp, top = 20.dp)) {
-        BackButton(onClickBack)
-        Spacer(modifier = Modifier.height(16.dp))
-        Text(text = "Title", fontSize = 20.sp)
-        Spacer(modifier = Modifier.height(8.dp))
-        Text(text = todo.title, fontSize = 15.sp)
-        Spacer(modifier = Modifier.height(16.dp))
-        Row(verticalAlignment = Alignment.CenterVertically) {
-            Text(text = "isDone", fontSize = 20.sp,modifier = Modifier.padding(end = 20.dp))
-            Checkbox(checked = todo.isChecked, onCheckedChange = {})
+fun DetailScreenRoute(todoTitle : String,onBackButtonClick : () -> Unit, viewModel : DetailViewModel = hiltViewModel()){
+    viewModel.fetchTodo(todoTitle)
+    DetailScreen(onBackButtonClick, viewModel::deleteTodo, todo = viewModel.todo.value)
+}
+@Composable
+fun DetailScreen(onBackButtonClick: () -> Unit, onDeleteTodoClick : () -> Unit, todo : Todo?) {
+    if(todo != null){
+        Column(modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 20.dp, top = 20.dp)) {
+            BackButton(onBackButtonClick)
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(text = "Title", fontSize = 20.sp)
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(text = todo.title, fontSize = 15.sp)
+            Spacer(modifier = Modifier.height(16.dp))
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Text(text = "isDone", fontSize = 20.sp,modifier = Modifier.padding(end = 20.dp))
+                Checkbox(checked = todo.isChecked, onCheckedChange = {})
+            }
+            Spacer(modifier = Modifier.height(30.dp))
+            DeleteButton(delete = {
+                onDeleteTodoClick()
+                onBackButtonClick()
+
+            })
         }
-        Spacer(modifier = Modifier.height(30.dp))
-        DeleteButton(delete = onClickDelete)
+    }else{
+        Text(text = "로딩중")
     }
+
 }
 
 @Composable
-fun DetailErrorScreen(back : () -> Unit){
-    Column(Modifier.fillMaxWidth().padding(start = 20.dp, top = 20.dp)) {
-        BackButton(back)
+fun DetailErrorScreen(onBackButtonClick : () -> Unit){
+    Column(
+        Modifier
+            .fillMaxWidth()
+            .padding(start = 20.dp, top = 20.dp)) {
+        BackButton(onBackButtonClick)
         Spacer(modifier = Modifier.height(16.dp))
         Text(text = "에러가 발생했습니다.", fontSize = 20.sp)
     }
@@ -43,5 +64,5 @@ fun DetailErrorScreen(back : () -> Unit){
 @Preview
 @Composable
 fun PreviewDetailScreen() {
-    DetailScreen(onClickBack = {},onClickDelete = {},todo=Todo("todo",false))
+    DetailScreen(onBackButtonClick = {}, onDeleteTodoClick = {},todo=Todo("todo",false))
 }
