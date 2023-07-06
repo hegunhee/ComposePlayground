5ba81c1c69de65e0ec0e6369900efc1c7e74f918
hegunhee <gunhee0072@gmail.com>
2023-07-06 22:33:18 +0900

    [compose-navigation] feat : Route들 object 타입의 프로퍼티로 재 지정

--- app/src/main/java/com/example/composeplayground/navigation/Screen.kt
+++ app/src/main/java/com/example/composeplayground/navigation/Screen.kt
@@ -2,18 +2,11 @@ package com.example.composeplayground.navigation
 
 import androidx.navigation.NavBackStackEntry
 
-sealed class Screen(val route : String){
+object TodoGraph {
 
-    object Todo : Screen("Todo"){
-        fun deleteArgument(entry : NavBackStackEntry) : String? {
-            return entry.arguments?.getString("deleteTodo")
-        }
-    }
-    object DetailTodo : Screen("Detail/{title}") {
-        fun createRoute(title : String) = "Detail/$title"
-        fun getArgumentTitle(entry : NavBackStackEntry) : String? {
-            return entry.arguments?.getString("title")
-        }
-    }
+    val todoRoute = "Todo"
 
+    val detailRoute = "Detail/{title}"
+
+    fun createDetailTodoRoute(title : String) = "Detail/$title"
 }
