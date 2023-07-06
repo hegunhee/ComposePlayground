48b4941fdc81a5cd6e5c6afd92686d85f1db14a2
hegunhee <gunhee0072@gmail.com>
2023-07-06 22:28:46 +0900

    [compose-navigation] feat : App의 NavHost 정의

---
+++ app/src/main/java/com/example/composeplayground/PlayGroundApp.kt
@@ -0,0 +1,48 @@
+package com.example.composeplayground
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import com.example.composeplayground.navigation.TodoGraph
+import com.example.composeplayground.ui.theme.ComposePlaygroundTheme
+import kotlinx.coroutines.CoroutineScope
+
+@Composable
+fun PlayGroundApp() {
+    ComposePlaygroundTheme {
+        val playGroundAppState = rememberPlayGroundAppState()
+        NavHost(modifier = Modifier, navController = playGroundAppState.navController, startDestination = TodoGraph.TodoRoute) {
+            todoScreen(playGroundAppState::navigateDetailTodo)
+            detailScreen(onBackStack = playGroundAppState::onBackStack)
+        }
+    }
+}
+
+@Composable
+fun rememberPlayGroundAppState(
+    coroutineScope: CoroutineScope = rememberCoroutineScope(),
+    navController: NavHostController = rememberNavController()
+) : PlayGroundAppState {
+    return remember(key1 = coroutineScope, key2 = navController) {
+        PlayGroundAppState(
+            coroutineScope,
+            navController
+        )
+    }
+}
+class PlayGroundAppState(
+    val coroutineScope : CoroutineScope,
+    val navController : NavHostController
+) {
+    fun navigateDetailTodo(title : String) {
+        navController.navigate(TodoGraph.createDetailTodoRoute(title))
+    }
+
+    fun onBackStack() {
+        navController.popBackStack()
+    }
+}
