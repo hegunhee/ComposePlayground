43c81b8f7d7ad7d957245d07bf6d77945b1406fd
hegunhee <gunhee0072@gmail.com>
2023-07-10 21:07:26 +0900

    [todo-app] refactor : Screen에서 todo를 nullable한값이 아닌 State로 관리

--- app/src/main/java/com/example/composeplayground/screen/DetailScreen.kt
+++ app/src/main/java/com/example/composeplayground/screen/DetailScreen.kt
@@ -13,41 +13,48 @@ import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.composeplayground.components.BackButton
 import com.example.composeplayground.todo.detail.DetailViewModel
+import com.example.composeplayground.todo.detail.TodoState
 import com.example.domain.model.Todo
 
 @Composable
 fun DetailScreenRoute(todoTitle : String,onBackButtonClick : () -> Unit, viewModel : DetailViewModel = hiltViewModel()){
     viewModel.fetchTodo(todoTitle)
-    DetailScreen(onBackButtonClick, viewModel::deleteTodo, todo = viewModel.todo.value)
+    when(val todoState = viewModel.todoState.value) {
+        TodoState.Loading -> {
+            DetailLoadingScreen()
+        }
+        is TodoState.Success -> {
+            DetailScreen(onBackButtonClick = onBackButtonClick, onDeleteTodoClick = viewModel::deleteTodo, todo = todoState.todo)
+        }
+    }
 }
 @Composable
-fun DetailScreen(onBackButtonClick: () -> Unit, onDeleteTodoClick : () -> Unit, todo : Todo?) {
-    if(todo != null){
-        Column(modifier = Modifier
-            .fillMaxWidth()
-            .padding(start = 20.dp, top = 20.dp)) {
-            BackButton(onBackButtonClick)
-            Spacer(modifier = Modifier.height(16.dp))
-            Text(text = "Title", fontSize = 20.sp)
-            Spacer(modifier = Modifier.height(8.dp))
-            Text(text = todo.title, fontSize = 15.sp)
-            Spacer(modifier = Modifier.height(16.dp))
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                Text(text = "isDone", fontSize = 20.sp,modifier = Modifier.padding(end = 20.dp))
-                Checkbox(checked = todo.isChecked, onCheckedChange = {})
-            }
-            Spacer(modifier = Modifier.height(30.dp))
-            Button(onClick = {
-                onDeleteTodoClick()
-                onBackButtonClick()
-            }) {
-                Text(text = "삭제",fontSize = 16.sp)
-            }
+fun DetailScreen(onBackButtonClick: () -> Unit, onDeleteTodoClick : (Todo) -> Unit, todo : Todo) {
+    Column(modifier = Modifier
+        .fillMaxWidth()
+        .padding(start = 20.dp, top = 20.dp)) {
+        BackButton(onBackButtonClick)
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(text = "Title", fontSize = 20.sp)
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(text = todo.title, fontSize = 15.sp)
+        Spacer(modifier = Modifier.height(16.dp))
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Text(text = "isDone", fontSize = 20.sp,modifier = Modifier.padding(end = 20.dp))
+            Checkbox(checked = todo.isChecked, onCheckedChange = {})
         }
-    }else{
-        Text(text = "로딩중")
+        Spacer(modifier = Modifier.height(30.dp))
+        Button(onClick = {
+            onDeleteTodoClick(todo)
+            onBackButtonClick()
+        }) {
+            Text(text = "삭제",fontSize = 16.sp)
+        }
     }
-
+}
+@Composable
+fun DetailLoadingScreen() {
+    Text(text = "로딩중")
 }
 
 @Composable
