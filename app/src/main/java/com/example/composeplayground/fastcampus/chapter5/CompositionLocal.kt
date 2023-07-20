4b311507313eb32c2edf037006a5e16066b6060a
hegunhee <gunhee0072@gmail.com>
2023-07-20 14:47:22 +0900

    [composition-local] feat : 배경색을 지정하는 compositionLocal 변수 생성

---
+++ app/src/main/java/com/example/composeplayground/fastcampus/chapter5/CompositionLocal.kt
@@ -0,0 +1,15 @@
+package com.example.composeplayground.fastcampus.chapter5
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.ui.graphics.Color
+
+val LocalColorComposition = compositionLocalOf { Color.Black }
+
+@Composable
+fun CompositionLocalApp() {
+    Column() {
+
+    }
+}
