815d4bd761b68c762f4281e3cf8c87b914d0c08d
hegunhee <gunhee0072@gmail.com>
2023-07-20 14:56:09 +0900

    [composition-local] feat : 색상 compositionLocal 테스트

--- app/src/main/java/com/example/composeplayground/fastcampus/chapter5/CompositionLocal.kt
+++ app/src/main/java/com/example/composeplayground/fastcampus/chapter5/CompositionLocal.kt
@@ -1,8 +1,13 @@
 package com.example.composeplayground.fastcampus.chapter5
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 
 val LocalColorComposition = compositionLocalOf { Color.Black }
@@ -10,6 +15,24 @@ val LocalColorComposition = compositionLocalOf { Color.Black }
 @Composable
 fun CompositionLocalApp() {
     Column() {
-
+        Text(text = "Color.Black",modifier = Modifier
+            .background(LocalColorComposition.current)
+            .fillMaxWidth())
+        CompositionLocalProvider(LocalColorComposition provides Color.Magenta) {
+            MagentaBackgroundText()
+        }
     }
+}
+
+@Composable
+fun MagentaBackgroundText() {
+    Text(text = "Color.Magenta",modifier = Modifier.background(LocalColorComposition.current).fillMaxWidth())
+    CompositionLocalProvider(LocalColorComposition provides Color.Blue) {
+        BlueBackgroundText()
+    }
+}
+
+@Composable
+fun BlueBackgroundText() {
+    Text(text = "Color.Blue",modifier = Modifier.background(LocalColorComposition.current).fillMaxWidth())
 }
