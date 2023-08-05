6ab5811f1628e616f82e3f5fd03fb4da3de4c565
hegunhee <gunhee0072@gmail.com>
2023-08-05 12:10:26 +0900

    [compose-layout] feat : 기본제약 예제

--- app/src/main/java/com/example/composeplayground/study/book/layout/ConstraintLayoutExample.kt
+++ app/src/main/java/com/example/composeplayground/study/book/layout/ConstraintLayoutExample.kt
@@ -1,6 +1,8 @@
 package com.example.composeplayground.study.book.layout
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -10,13 +12,20 @@ import androidx.constraintlayout.compose.ConstraintLayout
 
 @Composable
 fun ConstraintLayoutExample() {
-    ConstraintLayout {
-        val text1 = createRef()
+    ConstraintLayout(Modifier.size(width = 200.dp,height = 200.dp)) {
+        val (button1, button2, button3) = createRefs()
 
-        Text(text = "Hello",modifier = Modifier.constrainAs(text1) {
-            top.linkTo(parent.top,margin = 16.dp)
-            bottom.linkTo(parent.bottom,margin= 16.dp)
-            start.linkTo(parent.start,margin = 20.dp)
-        }.background(Color.Magenta))
+        CustomButton(text = "Button1", modifier = Modifier.constrainAs(button1) {
+            top.linkTo(parent.top,margin = 60.dp)
+            start.linkTo(parent.start, margin = 30.dp)
+        })
+
+    }
+}
+
+@Composable
+fun CustomButton(text : String = "",modifier :Modifier = Modifier) {
+    Button(onClick = { },modifier = modifier) {
+        Text(text = text)
     }
 }
